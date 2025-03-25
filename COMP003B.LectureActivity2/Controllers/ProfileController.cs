281f67af7ef5cb6348add57d51e8a514cb90166b
RobinGuyton <gladiatorrobin@aol.com>
2025-03-24 19:22:21 -0700

    Added Sidebar

---
+++ COMP003B.LectureActivity2/Controllers/ProfileController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.LectureActivity2.Controllers
+{
+    public class ProfileController : Controller
+    {
+        [HttpGet]
+        public IActionResult About()
+        
+        {
+            return View();
+
+        }
+
+
+        [HttpGet]
+
+        public IActionResult Hobbies()
+        {
+            return View();
+        }
+
+
+    }
+}
---
+++ COMP003B.LectureActivity2/Views/Shared/_Sidebar.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace COMP003B.LectureActivity2.Views.Shared
+{
+    public class _SidebarModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
